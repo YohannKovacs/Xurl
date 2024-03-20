c6a936bfdc70e7cc294aedc2926cd7c01aeaa0b1
Paul <rp185405@ncr.com>
2024-03-20 13:38:33 +0530

    Fixed error messages

--- main.go
+++ main.go
@@ -21,6 +21,12 @@ var (
 	data       = flags.Bool("data", false, "Add Json data to the request object.")
 )
 
+var (
+	addr			string
+	scheme			string
+	dataBytes		[]byte
+)
+
 func main() {
 	flags.Usage = usage
 	flags.Parse(os.Args[1:])
@@ -42,38 +48,113 @@ func main() {
 		fail(nil, "Too few arguments.")
 	}
 
-	addr := args[len(args)-1]
+	addr = strings.TrimSpace(args[len(args)-1])
 	if addr == "" {
 		fail(nil, "No host or port specified")
 	}
-
+	
 	// FIXME(Can we handle www better?)
 	if strings.Split(addr, ".")[0] == "www" {
 		addr, _ = strings.CutPrefix(addr, "www.")
 		addr = strings.Join([]string{"http", addr}, "://")
 	}
+	scheme = strings.TrimSpace(strings.Split(addr, "://")[0])
 
+	if *data {
+		dataArg := args[:len(args)-1]
+		if strings.ContainsRune(dataArg[0], '@') {
+			dataBytes, err := os.ReadFile(strings.Split(dataArg[0], "@")[1])
+			fmt.Println(string(dataBytes))
+			if err != nil {
+				fail(err, "Error while reading request data file")
+			}
+		}
+	}
+
+	if scheme != "" {
+		switch scheme {
+		case "http", "https":
+			defaultRequest()
+		case "ftp":
+		case "telnet":
+		case "imap":
+		default :
+		}
+	}
+}
+
+
+func usage() {
+	printLogo()
+	fmt.Fprintf(os.Stderr, prettify(`
+	Usage:
+	%s [flags] [data] [address]
+
+	Xurl is a curl alternative cli program. The main purpose of xurl is to send data 
+	to stdout to make it easy to chain it with other tools to do what you want.
+	`), os.Args[0])
+	fmt.Fprintln(os.Stderr)
+	flags.PrintDefaults()
+}
+
+func fail(err error, msg string, args ...interface{}) {
+	if err != nil {
+		msg += ": %v"
+		args = append(args, err)
+	}
+	fmt.Fprintf(os.Stderr, msg, args)
+	fmt.Fprintln(os.Stderr)
+	if err != nil {
+		exit(1)
+	} else {
+		// nil err means it was cli usage issue
+		fmt.Fprintf(os.Stderr, "Try %s -help for more details.\n", os.Args[0])
+		exit(2)
+	}
+}
+
+func prettify(docString string) string {
+	parts := strings.Split(docString, "\n")
+	j := 0
+
+	for _, part := range parts {
+		part = strings.TrimSpace(part)
+		if part == "" {
+			continue
+		}
+		parts[j] = part
+		j++
+	}
+	return strings.Join(parts[:j], "\n")
+}
+
+func printLogo() {
+	logo, err := os.ReadFile("logo.txt")
+	if err != nil {
+		fail(err, "Error while reading the logo file.")
+	}
+	fmt.Fprintln(os.Stderr, string(logo))
+}
+
+// TODO*()
+func ftpRequest() {
+
+}
+
+// TODO()
+func telnetRequest() {
+
+}
+
+// TODO(Makes a request on the basis of the default http scheme.)
+func defaultRequest() {
 	var (
 		resp *http.Response
 		err  error
 	)
 
-	// TODO(Parse the error and respond, if couldn't resolve host, or url malformed, or host failed to connect, etc.)
 	if *data {
-		var data []byte
-		dataArg := args[:len(args)-1]
-		if strings.ContainsRune(dataArg[0], '@') {
-			data, err = os.ReadFile(strings.Split(dataArg[0], "@")[1])
-			fmt.Println(string(data))
-			if err != nil {
-				fail(err, "Error while reading request data file")
-			}
-		}
-		resp, err = http.Post(addr, "Content-Type: application/json", bytes.NewBuffer(data))
-		if err != nil {
-			fail(err, "Error while getting a response.")
-		}
-
+		resp, err = http.Post(addr, "Content-Type: application/json", bytes.NewBuffer(dataBytes))
 	} else {
 		resp, err = http.Get(addr)
 	}
@@ -103,54 +184,7 @@ func main() {
 	}
 }
 
-func usage() {
-	printLogo()
-	fmt.Fprintf(os.Stderr, prettify(`
-	Usage:
-	%s [flags] [data] [address]
+// TODO(Makes a request on the basis of the imap scheme)
+func imapRequest() {
 
-	Xurl is a curl alternative cli program. The main purpose of xurl is to send data 
-	to stdout to make it easy to chain it with other tools to do what you want.
-	`), os.Args[0])
-	fmt.Fprintln(os.Stderr)
-	flags.PrintDefaults()
-}
-
-func fail(err error, msg string, args ...interface{}) {
-	if err != nil {
-		msg += ": %v"
-		args = append(args, msg)
-	}
-	fmt.Fprintf(os.Stderr, msg, args)
-	fmt.Fprintln(os.Stderr)
-	if err != nil {
-		exit(1)
-	} else {
-		fmt.Fprintf(os.Stderr, "Try %s -help for more details.\n", os.Args[0])
-		exit(2)
-	}
-}
-
-func prettify(docString string) string {
-	parts := strings.Split(docString, "\n")
-	j := 0
-
-	for _, part := range parts {
-		part = strings.TrimSpace(part)
-		if part == "" {
-			continue
-		}
-		parts[j] = part
-		j++
-	}
-	return strings.Join(parts[:j], "\n")
-}
-
-func printLogo() {
-	logo, err := os.ReadFile("logo.txt")
-	if err != nil {
-		fail(err, "Error while reading the logo file.")
-	}
-
-	fmt.Fprintln(os.Stderr, string(logo))
 }
